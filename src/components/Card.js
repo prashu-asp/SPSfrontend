3c3fb1dfa88a2150788a3859b5abbcc01d25835f
prashu-asp <92110486+prashu-asp@users.noreply.github.com>
2022-01-14 17:14:30 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -27,17 +27,6 @@ const Card = (props) => {
     );
 }
 
-// const Card = ({name, email, id})/*destructing*/ => {
-//     return (
-//         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-//             <img alt='photo' src={`https://robohash.org/${id}?200x200`} />
-//             <div>
-//                 <h2>{name}</h2>
-//                 <p>{email}</p>
-//             </div>
-//         </div>
-//     );
-// }
 
 
 
