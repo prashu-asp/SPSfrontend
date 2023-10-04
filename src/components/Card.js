63a57b917fbf64b0ba6fb64b5e9dcd89518df747
A SAI PRASHANTH <92110486+prashu-asp@users.noreply.github.com>
2023-10-04 16:18:24 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ import Scr6 from '../images/Scr6.jpeg'
 import Scr7 from '../images/Scr7.jpeg'
 import Scr8 from '../images/Scr8.jpg'
 import Scr9 from '../images/Scr9.jpeg'
-import Scr10 from '../images/Scr10.jpeg'
+import SRC1 from '../images/SRC1.png'
 import './card.css'
 const Card = (props) => {
 
@@ -16,7 +16,7 @@ const Card = (props) => {
     
     return (
         <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
-            <img style={{height :200 , width : 200}} alt='photo' src={Scr1} />
+            <img style={{height :200 , width : 200}} alt='photo' src={SRC1} />
             <div>
                 <a href="#" className={props.name.length <= 20 ? 'smallname' : 'bigname'}>{props.name.slice(0,23)}</a><br />
                 <a href="#" className={props.name.length <= 20 ? 'smallname' : 'bigname'}>{props.name.slice(23,46)} </a><br />
